55d4e8b774e6b2d6db27c304579e1bde1e90fcec
reki2000 <2533597+reki2000@users.noreply.github.com>
2024-07-28 12:20:15 +0900

    600x400

--- main.c
+++ main.c
@@ -21,84 +21,103 @@
 
 extern int render(char *rgb_out, int nx, int ny, float camera_x, float camera_y, float camera_z);
 
-
 XImage *create_ximage(Display *display, Visual *visual, int width, int height, float camera_x, float camera_y, float camera_z)
 {
-        char *image32 = (char *)malloc(width * height * 4);
-        render(image32, width, height, camera_x, camera_y, camera_z);
-        return XCreateImage(display, visual, 24,
-                            ZPixmap, 0, image32,
-                            width, height, 32, 0);
+    char *image32 = (char *)malloc(width * height * 4);
+    render(image32, width, height, camera_x, camera_y, camera_z);
+    return XCreateImage(display, visual, 24,
+                        ZPixmap, 0, image32,
+                        width, height, 32, 0);
 }
 
 int main(int argc, char **argv)
 {
-        int win_b_color;
-        int win_w_color;
-        XEvent xev;
-        Window window;
-        GC gc;
-        Display *display = XOpenDisplay(NULL);
-        Visual *visual;
-        XImage *ximage;
-        KeySym key_sym;
-        float camera_x = 0.0; 
-        float camera_y = 0.0;
-        float camera_z = 0.0;
+    int win_b_color;
+    int win_w_color;
+    XEvent xev;
+    Window window;
+    GC gc;
+    Display *display = XOpenDisplay(NULL);
+    Visual *visual;
+    XImage *ximage;
+    KeySym key_sym;
 
-        win_b_color = BlackPixel(display, DefaultScreen(display));
-        win_w_color = BlackPixel(display, DefaultScreen(display));
-        window = XCreateSimpleWindow(display,
-                                DefaultRootWindow(display),
-                                0, 0, 1200, 800, 0,
-                                win_b_color, win_w_color);
+    int image_width = 600;
+    int image_height = 400;
+    float camera_x = 0.0;
+    float camera_y = 0.0;
+    float camera_z = 0.0;
 
-        visual = DefaultVisual(display, 0);
+    win_b_color = BlackPixel(display, DefaultScreen(display));
+    win_w_color = BlackPixel(display, DefaultScreen(display));
+    window = XCreateSimpleWindow(display,
+                                 DefaultRootWindow(display),
+                                 0, 0, image_width, image_height, 0,
+                                 win_b_color, win_w_color);
 
-        XSelectInput(display, window, ExposureMask | KeyPressMask);
+    visual = DefaultVisual(display, 0);
 
-        XMapWindow(display, window);
-        XFlush(display);
-        gc = XCreateGC(display, window, 0, NULL);
-        XEvent event;
-        bool exit = false;
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
 
-        while (!exit) {
-                int r;
+    XMapWindow(display, window);
+    XFlush(display);
+    gc = XCreateGC(display, window, 0, NULL);
+    XEvent event;
+    bool exit = false;
 
-                XNextEvent(display, &event);
+    while (!exit)
+    {
+        int r;
 
-                if (event.type == Expose)
-                {
-                    ximage = create_ximage(display, visual, 1200, 800, camera_x, camera_y, camera_z);
-                    r = XPutImage(display, window, gc, ximage, 0, 0, 0, 0, 1200, 800);
-                    continue;
-                }
-                else if (event.type == KeyPress) {
-                    key_sym = XkbKeycodeToKeysym( display, event.xkey.keycode, 0, event.xkey.state & ShiftMask ? 1 : 0);
-                    if( key_sym == XK_Escape ){
-                        XDestroyWindow(display, window);
-                        exit=true;
-                        continue;
-                    } else if (key_sym == XK_Right) {
-                        camera_x += 0.1;
-                    } else if (key_sym == XK_Left) {
-                        camera_x -= 0.1;
-                    } else if (key_sym == XK_Up) {
-                        camera_y += 0.1;
-                    } else if (key_sym == XK_Down) {
-                        camera_y -= 0.1;
-                    } else if (key_sym == XK_w) {
-                        camera_z += 0.1;
-                    } else if (key_sym == XK_s) {
-                        camera_z -= 0.1;
-                    } else {
-                        continue;
-                    }
-                    ximage = create_ximage(display, visual, 1200, 800, camera_x, camera_y, camera_z);
-                    r = XPutImage(display, window, gc, ximage, 0, 0, 0, 0, 1200, 800);
-                }
+        XNextEvent(display, &event);
+
+        if (event.type == Expose)
+        {
+            ximage = create_ximage(display, visual, image_width, image_height, camera_x, camera_y, camera_z);
+            r = XPutImage(display, window, gc, ximage, 0, 0, 0, 0, 1200, 800);
+            continue;
         }
+        else if (event.type == KeyPress)
+        {
+            key_sym = XkbKeycodeToKeysym(display, event.xkey.keycode, 0, event.xkey.state & ShiftMask ? 1 : 0);
+            if (key_sym == XK_Escape)
+            {
+                XDestroyWindow(display, window);
+                exit = true;
+                continue;
+            }
+            else if (key_sym == XK_Right)
+            {
+                camera_x += 0.1;
+            }
+            else if (key_sym == XK_Left)
+            {
+                camera_x -= 0.1;
+            }
+            else if (key_sym == XK_Up)
+            {
+                camera_y += 0.1;
+            }
+            else if (key_sym == XK_Down)
+            {
+                camera_y -= 0.1;
+            }
+            else if (key_sym == XK_w)
+            {
+                camera_z += 0.1;
+            }
+            else if (key_sym == XK_s)
+            {
+                camera_z -= 0.1;
+            }
+            else
+            {
+                continue;
+            }
+            ximage = create_ximage(display, visual, image_width, image_height, camera_x, camera_y, camera_z);
+            r = XPutImage(display, window, gc, ximage, 0, 0, 0, 0, 1200, 800);
+        }
+    }
 
-        return 0;
+    return 0;
 }
--- main.cu
+++ main.cu
@@ -58,7 +58,7 @@ __global__ void rand_init(curandState *rand_state) {
     }
 }
 
-__global__ void render_init(int max_x, int max_y, curandState *rand_state, int ns, size_t fb_size) {
+__global__ void render_init(int max_x, int max_y, curandState *rand_state) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
@@ -67,41 +67,31 @@ __global__ void render_init(int max_x, int max_y, curandState *rand_state, int n
     // curand_init(1984, pixel_index, 0, &rand_state[pixel_index]);
     // BUGFIX, see Issue#2: Each thread gets different seed, same sequence for
     // performance improvement of about 2x!
-    for (int s=0; s<ns; s++) { 
-        curand_init(1984+pixel_index+s*fb_size, 0, 0, &rand_state[pixel_index+s*fb_size]);
-    }
+    curand_init(1984+pixel_index, 0, 0, &rand_state[pixel_index]);
 }
 
-__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state, size_t fb_size) {
+__global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
-    int s = threadIdx.z;
-    int pixel_index = j*max_x + i;
-    // for(int s=0; s < ns; s++) {
-        curandState local_rand_state = rand_state[pixel_index + s*fb_size];
-        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
-        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
-        ray r = (*cam)->get_ray(u, v, &local_rand_state);
-        fb[pixel_index * ns + s] = color(r, world, &local_rand_state);
-        rand_state[pixel_index + s*fb_size] = local_rand_state;
-    // }
-}
 
-__global__ void render2(vec3 *fb, int max_x, int max_y, int ns, camera **cam, hitable **world, curandState *rand_state, size_t fb_size) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
-    if((i >= max_x) || (j >= max_y)) return;
     int pixel_index = j*max_x + i;
+    curandState local_rand_state = rand_state[pixel_index];
 
     vec3 col(0,0,0);
     for(int s=0; s < ns; s++) {
-        col += fb[pixel_index + s*fb_size];
+        float u = float(i + curand_uniform(&local_rand_state)) / float(max_x);
+        float v = float(j + curand_uniform(&local_rand_state)) / float(max_y);
+        ray r = (*cam)->get_ray(u, v, &local_rand_state);
+        col += color(r, world, &local_rand_state);
     }
+    rand_state[pixel_index] = local_rand_state;
+
     col /= float(ns);
     col[0] = sqrt(col[0]);
     col[1] = sqrt(col[1]);
     col[2] = sqrt(col[2]);
+
     fb[pixel_index] = col;
 }
 
@@ -163,13 +153,13 @@ extern "C" int render(char *rgb_out, int nx, int ny, float camera_x, float camer
     // int nx = 1200;
     // int ny = 800;
     int ns = 10;
-    int tx = 8;
-    int ty = 8;
+    int tx = 16;
+    int ty = 16;
 
     std::cerr << "Rendering a " << nx << "x" << ny << " image with " << ns << " samples per pixel.. ";
 
     int num_pixels = nx*ny;
-    size_t fb_size = ns*num_pixels*sizeof(vec3);
+    size_t fb_size = num_pixels*sizeof(vec3);
 
     // allocate FB
     vec3 *fb;
@@ -177,7 +167,7 @@ extern "C" int render(char *rgb_out, int nx, int ny, float camera_x, float camer
 
     // allocate random state
     curandState *d_rand_state;
-    checkCudaErrors(cudaMalloc((void **)&d_rand_state, ns*num_pixels*sizeof(curandState)));
+    checkCudaErrors(cudaMalloc((void **)&d_rand_state, num_pixels*sizeof(curandState)));
     curandState *d_rand_state2;
     checkCudaErrors(cudaMalloc((void **)&d_rand_state2, 1*sizeof(curandState)));
 
@@ -200,17 +190,16 @@ extern "C" int render(char *rgb_out, int nx, int ny, float camera_x, float camer
 
     std::cerr << "in " << tx << "x" << ty << " blocks.. ";
 
-    clock_t start, stop;
-    start = clock();
     // Render our buffer
     dim3 blocks(nx/tx+1,ny/ty+1);
-    dim3 threads(tx,ty,ns);
-    dim3 threads2(tx,ty);
-    render_init<<<blocks, threads>>>(nx, ny, d_rand_state, ns, num_pixels);
+    dim3 threads(tx,ty);
+    render_init<<<blocks, threads>>>(nx, ny, d_rand_state);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
-    render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state, num_pixels);
-    //render2<<<blocks, threads2>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state, num_pixels);
+
+    clock_t start, stop;
+    start = clock();
+    render<<<blocks, threads>>>(fb, nx, ny,  ns, d_camera, d_world, d_rand_state);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
     stop = clock();
@@ -223,15 +212,9 @@ extern "C" int render(char *rgb_out, int nx, int ny, float camera_x, float camer
         for (int i = 0; i < nx; i++) {
             size_t pixel_index = j*nx + i;
             size_t dest = (ny-j-1)*nx + i;
-
-            vec3 col(0,0,0);
-            for(int s=0; s < ns; s++) {
-                col += fb[pixel_index*ns + s];
-            }
-            col /= float(ns);
-            rgb_out[dest*4 + 0] = int(255.99*sqrt(col.b()));
-            rgb_out[dest*4 + 1] = int(255.99*sqrt(col.g()));
-            rgb_out[dest*4 + 2] = int(255.99*sqrt(col.r()));
+            rgb_out[dest*4 + 0] = int(255.99*fb[pixel_index].b());
+            rgb_out[dest*4 + 1] = int(255.99*fb[pixel_index].g());
+            rgb_out[dest*4 + 2] = int(255.99*fb[pixel_index].r());
             // int ir = int(255.99*fb[pixel_index].r());
             // int ig = int(255.99*fb[pixel_index].g());
             // int ib = int(255.99*fb[pixel_index].b());
